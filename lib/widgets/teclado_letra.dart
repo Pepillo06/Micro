643789b743da30a10130796089d21f3aecd6acb2
gloriafer2 <gloria.fernandes@correo.unimet.edu.ve>
2025-09-27 18:41:06 -0400

    Terminando teclado

--- lib/widgets/teclado_letra.dart
+++ lib/widgets/teclado_letra.dart
@@ -14,4 +14,44 @@ const TecladoLetra({
     required this.equivocada,
     required this.onPresionar,
   });
+
+
+   @override
+  Widget build(BuildContext context) {
+    Color colorFondo;
+    
+    if (adivinada) {
+      colorFondo = Colors.green.shade700;
+    } else if (equivocada) {
+      colorFondo = Colors.red.shade700;
+    } else {
+      colorFondo = Colors.deepPurple;
+    }
+
+    return SizedBox(
+      width: 50,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: habilitado
+            ? () => onPresionar(letra)
+            : null,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: colorFondo,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: EdgeInsets.zero,
+          disabledBackgroundColor: colorFondo, 
+        ),
+        child: Text(
+          letra,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
+
+
+
