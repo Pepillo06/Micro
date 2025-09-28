6a39a231fb28ec43731ea35a768adb9c27078126
Pecathf <167771326+Pecathf@users.noreply.github.com>
2025-09-28 11:48:51 -0400

    Cambios en libreria de main
    Nuevos cambios

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'El Ahorcado',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // CORREGIDO: Usamos ColorScheme en lugar de primarySwatch
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        // fontFamily y primarySwatch eliminados.
         useMaterial3: true,
       ),
-      // Agregamos el 'const' que faltaba aquí
       home: const JuegoAhorcadoPage(),
     );
   }
