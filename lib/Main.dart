c3c38fcf49d10f126a2ba67c6872f395e3653a7f
gloriafer2 <gloria.fernandes@correo.unimet.edu.ve>
2025-09-28 18:35:07 -0400

    Update Main.dart

--- lib/Main.dart
+++ lib/Main.dart
@@ -1,1 +1,30 @@
 
+import 'package:flutter/material.dart';
+import 'juego_ahorcado_page.dart';
+
+import 'package:shared_preferences/shared_preferences.dart'; 
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ @override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    title: 'El Ahorcado',
+    debugShowCheckedModeBanner: false, 
+    theme: ThemeData(
+      
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), 
+    
+      useMaterial3: true,
+    ),
+   
+    home: const JuegoAhorcadoPage(), 
+  );
+}
+}
