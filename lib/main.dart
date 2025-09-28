342b4e6db3cd0abc5500662e27a046a4f00e2af0
Pepillo06 <jabrieliz.cuello@correo.unimet.edu.ve>
2025-09-28 18:22:36 -0400

    Up dart
    Actualizacion del main dart. No tomar en cuenta ya que es de prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
 import 'juego_ahorcado_page.dart';
 
-import 'package:shared_preferences/shared_preferences.dart'; 
-
 void main() {
-  WidgetsFlutterBinding.ensureInitialized(); 
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- @override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'El Ahorcado',
-    debugShowCheckedModeBanner: false, 
-    theme: ThemeData(
-      // CORREGIDO: Usamos ColorScheme en lugar de primarySwatch
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), 
-      // fontFamily y primarySwatch eliminados.
-      useMaterial3: true,
-    ),
-    // Agregamos el 'const' que faltaba aquí
-    home: const JuegoAhorcadoPage(), 
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'El Ahorcado',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple, useMaterial3: true),
+      home: JuegoAhorcadoPage(),
+    );
+  }
 }
