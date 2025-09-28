c6c472e6533feaae16341bd726976095333b742e
gloriafer2 <gloria.fernandes@correo.unimet.edu.ve>
2025-09-28 16:28:02 -0400

    mejorando

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,12 @@ Widget build(BuildContext context) {
     title: 'El Ahorcado',
     debugShowCheckedModeBanner: false, 
     theme: ThemeData(
-      // CORREGIDO: Usamos ColorScheme en lugar de primarySwatch
+      
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), 
-      // fontFamily y primarySwatch eliminados.
+    
       useMaterial3: true,
     ),
-    // Agregamos el 'const' que faltaba aquí
+   
     home: const JuegoAhorcadoPage(), 
   );
 }
